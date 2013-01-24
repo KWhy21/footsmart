6b0c223b8c35f525d1e3d9716b1d0ce97b68c942
Kris Young <kyoung9@hotmail.com>
2013-01-24 15:26:55 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Footsmart::Application.config.secret_token = '4e2ad8a2edb0012a7a7bf0858f07889dec4692ed5c4fc9b08da35d551357b4ae1b02d0b24be79921567981e68aac8b66806d1a3f9fe80cebcd3ef8253a17d613'
